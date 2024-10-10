7166675e2e2ada5ff507dfb0d905609842662135
Fantastik4k <40822174+Fantastik4k@users.noreply.github.com>
2024-10-10 18:34:01 +0500

    added notes for video 8 04_comparision.js

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -4,18 +4,23 @@
 // console.log(2 == 1);
 // console.log(2 != 1);
 
+// oper waly comparisons to simple hein, ya true hoga ya false. problem tub ati hy jub datatypes different hoon. to aap try krein k jb bhi aap comparison kr rhy hoon, confuse result/output sy bchny k liye dono operands ki datatypes same hoon.
+// console.log("2" > 1); output true
+// console.log("02" > 1); output true
 
-// console.log("2" > 1);
-// console.log("02" > 1);
+console.log(null > 0); // output false qk null to empty value hy
+console.log(null == 0); // output true
+console.log(null >= 0); // output true
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+The reason is that an equality check == and comparisons > < >= <= work differently.
+Comparisons convert null to a number, treating it as 0. That's why (3) null >= 0 is true and (1) null > 0 is false.
 
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0);
 
-// === 
 
-console.log("2" === 2);
+console.log(undefined == 0); // output false
+console.log(undefined > 0);  // output false
+console.log(undefined < 0); // output false
+
+// ===  i.e strict check
+
+console.log("2" === 2); // output false
