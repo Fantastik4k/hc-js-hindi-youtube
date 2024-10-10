9c45ce9fa75e73bdaeb529b55e47419167cc5b1b
Fantastik4k <40822174+Fantastik4k@users.noreply.github.com>
2024-10-10 23:26:50 +0500

    added notes for video 14

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,4 +1,4 @@
-// array
+// array -- arrrays in js are resizeable and can contain a mix of different data types.
 
 const myArr = [0, 1, 2, 3, 4, 5]
 const myHeors = ["shaktiman", "naagraj"]
@@ -11,29 +11,32 @@ const myArr2 = new Array(1, 2, 3, 4)
 // myArr.push(6)
 // myArr.push(7)
 // myArr.pop()
+// console.log(myarr) -- [0, 1, 2, 3, 4, 5, 6]
 
-// myArr.unshift(9)
-// myArr.shift()
+// myArr.unshift(9) -- adds 9 at the start of array, but it is not efficient, assume 1 million enteries in array, all have to be taken to one place further.
+// myArr.shift() -- first wali ko remove kr dey ga.
 
 // console.log(myArr.includes(9));
-// console.log(myArr.indexOf(3));
+// console.log(myArr.indexOf(3)); -- if 3 is in array, it will give us its first occurrence index. if no 3, then it gives us -1
 
 // const newArr = myArr.join()
 
-// console.log(myArr);
-// console.log( newArr);
+// console.log(myArr); [0, 1, 2, 3, 4, 5]
+// console.log( newArr); 0, 1, 2, 3, 4, 5 --if we check its type, we get string.
 
 
-// slice, splice
+// slice, splice -- 
 
-console.log("A ", myArr);
+const ahmArr = [0, 1, 2, 3, 4, 5]
 
-const myn1 = myArr.slice(1, 3)
+console.log("A ", ahmArr);
+
+const myn1 = ahmArr.slice(1, 3)
 
 console.log(myn1);
-console.log("B ", myArr);
+console.log("B ", ahmArr);
 
 
-const myn2 = myArr.splice(1, 3)
-console.log("C ", myArr);
-console.log(myn2);
+const myn2 = mahmArr.splice(1, 3) // idhr 1, 3 ka matlub yeh h k 1 index sy ley kr 3(including) index values ko myn2 mein daal do.  AUR AB MAIN DIFFERENCE B/W SLICE AND SPLICE. AB JUB YEH myn2 MEIN DAAL DIYE TO AB YEH KRO K ORIGNAL ARRAY, YAANI ahmArr KO MANIPULATE KR DO, YAANI JO BAQI VALUES HEIN Wo ahmArr MEIN DAAL DO.
+console.log("C ", ahmArr); // output C [0, 4, 5]
+console.log(myn2); [1, 2, 3]
