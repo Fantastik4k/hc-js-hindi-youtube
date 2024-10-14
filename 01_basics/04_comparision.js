8f876e7cb51fc8aecd491595cbb3551d285ea9ce
Fantastik4k <40822174+Fantastik4k@users.noreply.github.com>
2024-10-14 20:10:10 +0500

    some corrections.
    console.log(null == 0) // reutrn false, while it was written true.
    some code was not commited.

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -9,11 +9,11 @@
 // console.log("02" > 1); output true
 
 console.log(null > 0); // output false qk null to empty value hy
-console.log(null == 0); // output true
+console.log(null == 0); // output false
 console.log(null >= 0); // output true
 
-The reason is that an equality check == and comparisons > < >= <= work differently.
-Comparisons convert null to a number, treating it as 0. That's why (3) null >= 0 is true and (1) null > 0 is false.
+// The reason is that an equality check == and comparisons > < >= <= work differently.
+//Comparisons convert null to a number, treating it as 0. That's why (3) null >= 0 is true and (1) null > 0 is false.
 
 
 
