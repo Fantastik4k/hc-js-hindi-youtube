43ab25daa6e925dbacb5bce8cee66b10200ae12d
Fantastik4k <40822174+Fantastik4k@users.noreply.github.com>
2024-10-14 21:37:04 +0500

    added some code

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -19,8 +19,25 @@ console.log(gameName.indexOf('t')); // 2
 const newString = gameName.substring(0, 4) // idhr hum agr 0 ki jgh -8 likh dein, to substring usko ignore kr dey ga, aur 0 sy hi skiru kry ga.
 console.log(newString); // hite
 
-const anotherString = gameName.slice(-8, 4)
+const anotherString = gameName.slice(-8, 7) // last sy 8 chas ginon aur udhr sy substring start, start sy 7 tk gino, 6 tk end. output h-
 console.log(anotherString);
+
+/*
+const str = 'The quick brown fox jumps over the lazy dog.';
+
+console.log(str.slice(31));
+// Expected output: "the lazy dog."
+
+console.log(str.slice(4, 19)); // chothy sy start krna hy aur aur 18 tk
+// Expected output: "quick brown fox"
+
+console.log(str.slice(-7)); // last sy 7 characters ginon, aur likh do
+// Expected output: "zy dog."
+
+console.log(str.slice(-9, -5)); // last sy 9 characters aur 5 characters ginon, aur start 9 sy kro aur stop 5 py kr do
+// Expected output: "lazy"
+
+*/
 
 const newStringOne = "   hitesh    "
 console.log(newStringOne);
