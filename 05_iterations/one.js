c1ffab3f9ace92e61aba3578fc66c6e048092d51
Fantastik4k <40822174+Fantastik4k@users.noreply.github.com>
2024-10-21 07:23:38 +0500

    added notes for video 27

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -8,19 +8,19 @@ for (let i = 0; i <= 10; i++) {
     //console.log(element);
     
 }
-
-// console.log(element);
+// console.log(i) -- error aye ga, aur ana bhi chahye, q k yeh loop k andar define hwa hy, bahire accessible ni ho ga.
+// console.log(element); -- errror
 
 for (let i = 1; i <= 10; i++) {
     //console.log(`Outer loop value: ${i}`);
-   for (let j = 1; j <= 10; j++) {
+   for (let  j = 1; j <= 10; j++) {
     //console.log(`Inner loop value ${j} and inner loop ${i}`);
     //console.log(i + '*' + j + ' = ' + i*j );
    }
     
 }
 let myArray = ["flash", "batman", "superman"]
-//console.log(myArray.length);
+//console.log(myArray.length); -- nechy condition check krty hwy agr hum ny <= likh dein to hmy sary elements print krwany k baad underined bhi mily ga, jo k shi cheez nhi hy. qk length 3 hy jb k indexing 0 sy start hoti hy.
 for (let index = 0; index < myArray.length; index++) {
     const element = myArray[index];
     //console.log(element);
@@ -39,6 +39,16 @@ for (let index = 0; index < myArray.length; index++) {
     
 // }
 
+// output
+/*
+Value of i is 1
+Value of i is 2
+Value of i is 3
+Value of i is 4
+Detected 5
+*/
+
+// continue ka hum iss truh bhi keh skty hein k aik dfa maafi kr do.
 for (let index = 1; index <= 20; index++) {
     if (index == 5) {
         console.log(`Detected 5`);
@@ -47,3 +57,15 @@ for (let index = 1; index <= 20; index++) {
    console.log(`Value of i is ${index}`);
     
 }
+
+// output
+/*
+Value of i is 1
+Value of i is 2
+Value of i is 3
+Value of i is 4
+Detected 5
+Value of i is 6
+////////////  7
+till 20
+*/
