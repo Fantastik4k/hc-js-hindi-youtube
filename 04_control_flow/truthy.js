6ff8d86169e009d265a682e5edcb2f8d013b625e
Fantastik4k <40822174+Fantastik4k@users.noreply.github.com>
2024-10-21 07:03:05 +0500

    notes added for video 26

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -10,8 +10,10 @@ if (userEmail) {
 
 // false, 0, -0, BigInt 0n, "", null, undefined, NaN
 
-//truthy values
+//truthy values -- empty function bhi truthy value hy.
 // "0", 'false', " ", [], {}, function(){}
+
+// array aur object ko check krny ka tariqa k kia wo empty hein k nhi.
 
 // if (userEmail.length === 0) {
 //     console.log("Array is empty");
@@ -23,15 +25,18 @@ if (Object.keys(emptyObj).length === 0) {
     console.log("Object is empty");
 }
 
+// false == 0 --true
+// false == "" --ture
+// 0 == "" --true
+
+
 // Nullish Coalescing Operator (??): null undefined
 
 let val1;
-// val1 = 5 ?? 10
-// val1 = null ?? 10
-// val1 = undefined ?? 15
-val1 = null ?? 10 ?? 20
-
-
+// val1 = 5 ?? 10 -- val1 mein 5 a jaye ga.
+// val1 = null ?? 10 -- val1 mein 10 a jaye ga.
+// val1 = undefined ?? 15 -- val1 mein 15 a jaye ga
+val1 = null ?? 10 ?? 20 -- valu1 mein 10 assign ho jaye ga.
 
 console.log(val1);
 
@@ -41,3 +46,5 @@ console.log(val1);
 
 const iceTeaPrice = 100
 iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
+
+// you can see last 5 minutes of video number 26 to reivew all the concepts explained in video.
