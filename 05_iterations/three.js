420af82e11a28fe9ea382f02592a95766a1a16df
Fantastik4k <40822174+Fantastik4k@users.noreply.github.com>
2024-10-21 08:19:36 +0500

    notes added for video 29

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -1,3 +1,4 @@
+// idhr hum jo loops prhny ja rhy hein, yeh thory sy array specific hein. I think yeh dosri data structures py bhi lga skty hein, but inka mostly use array k sath hi hota hy. strings/arrays/ object => yeh iterators hein, inn py aap loop lga skty hein.
 // for of
 
 // ["", "", ""]
@@ -23,11 +24,14 @@ map.set('Fr', "France")
 map.set('IN', "India")
 
 
-// console.log(map);
+// console.log(map); -- amps mein unique values hoti hein, jo order dia hota hy, usi order mein ati hein. idhr ab jb iss map ko print krein gy to ouput mein india sirf aik hi baar aye ga.
 
 for (const [key, value] of map) {
     // console.log(key, ':-', value);
 }
+
+// 'const [key, value] of map', yeh likhny sy hum ny actually mein array ko destruct kr lia hy.
+// agr hum sirf "const key of map" print krwaty to ['IN', 'India'] ['USA', 'United States of America'] ['Fr', 'France']
 
 const myObject = {
     game1: 'NFS',
@@ -38,3 +42,5 @@ const myObject = {
 //     console.log(key, ':-', value);
     
 // }
+
+// idhr error hi aye ga. k myObject is not iterable. q k objects ko iterate krny k liye hum for in loop ka use krty hein
