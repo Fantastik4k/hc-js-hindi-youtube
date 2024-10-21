ecd1c09bc1294647608ed12101c417645c4b2bb8
Fantastik4k <40822174+Fantastik4k@users.noreply.github.com>
2024-10-21 09:51:00 +0500

    added notes for video 30

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -1,10 +1,13 @@
 const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-// const newNums = myNumers.map( (num) => { return num + 10})
+// const newNums = myNumers.map( (num) => { return num + 10}) --output  [ 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 ]
 
 const newNums = myNumers
                 .map((num) => num * 10 )
                 .map( (num) => num + 1)
                 .filter( (num) => num >= 40)
 
-console.log(newNums);
+// ab jo dosra map hy us mein num ki values pehly map ki resulted values hoon gi.
+
+console.log(newNums); 
+
